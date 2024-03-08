3a5854693a7242a9a35436c35c84dc4aee556c93
DustinFader <dustinfader16@gmail.com>
2024-03-08 01:18:21 -0400

    refactor to have the turns be in the game class.

--- 01_game.rb
+++ 01_game.rb
@@ -1,7 +1,57 @@
 class Game
-	attr_accessor :current_player
 
 	def initialize
 		@current_player = 0
+	end
+
+	def turn
+		player_1 = Player.new(1)
+  	player_2 = Player.new(2)
+  	players = [player_1, player_2]
+
+  	while true
+			num1 = 1 + rand(19)
+			num2 = 1 + rand(19)
+			question = [num1, num2, num1 + num2]
+
+			puts "Player #{players[@current_player].name}: What does #{question[0]} + #{question[1]} equal?"
+			print ">"
+			player_answer = $stdin.gets.chomp
+
+
+			if (player_answer != "#{question[2]}")
+				# subtract current player life
+				puts "Player #{players[@current_player].name}: Seriously? No!"
+
+				players[@current_player].lives -= 1
+
+				
+				if players[@current_player].lives <= 0
+					if @current_player == 0
+						@current_player = 1
+					else
+						@current_player = 0
+					end
+					
+					puts "Player #{players[@current_player].name} wins with a score of #{players[@current_player].lives}/3"
+					puts "----- GAME OVER -----"
+					puts "Good bye!"
+					exit(0)
+				else
+					puts "P1: #{players[0].lives}/3 vs P2: #{players[1].lives}/3"
+				end
+				
+				puts "--- NEW TURN ---"
+			else
+				puts "Player #{players[@current_player].name}: Yes! You are correct."
+				puts "--- NEW TURN ---"
+			end
+			
+			if @current_player == 0
+				@current_player = 1
+			else
+				@current_player = 0
+			end
+		end
 	end
 end
--- main.rb
+++ main.rb
@@ -1,57 +1,6 @@
-require './02_player'
 require './01_game'
+require './02_player'
 
-def game
-  player_1 = Player.new(1)
-  player_2 = Player.new(2)
-  players = [player_1, player_2]
+game_instance = Game.new
 
-  game_instance = Game.new
-
-  while true
-    num1 = 1 + rand(19)
-    num2 = 1 + rand(19)
-    question = [num1, num2, num1 + num2]
-
-    puts "Player #{players[game_instance.current_player].name}: What does #{question[0]} + #{question[1]} equal?"
-    print ">"
-    player_answer = $stdin.gets.chomp
-
-
-    if (player_answer != "#{question[2]}")
-      # subtract current player life
-      puts "Player #{players[game_instance.current_player].name}: Seriously? No!"
-
-      players[game_instance.current_player].lives -= 1
-
-      
-      if players[game_instance.current_player].lives <= 0
-        if game_instance.current_player == 0
-          game_instance.current_player = 1
-        else
-          game_instance.current_player = 0
-        end
-        
-        puts "Player #{players[game_instance.current_player].name} wins with a score of #{players[game_instance.current_player].lives}/3"
-        puts "----- GAME OVER -----"
-        puts "Good bye!"
-        exit(0)
-      else
-        puts "P1: #{players[0].lives}/3 vs P2: #{players[1].lives}/3"
-      end
-      
-      puts "--- NEW TURN ---"
-    else
-      puts "Player #{players[game_instance.current_player].name}: Yes! You are correct."
-      puts "--- NEW TURN ---"
-    end
-    
-    if game_instance.current_player == 0
-      game_instance.current_player = 1
-    else
-      game_instance.current_player = 0
-    end
-  end
-end
-
-game
+game_instance.turn
