ff5a9bf41c2937511324aed45dcbbdac5cbcbe79
DustinFader <dustinfader16@gmail.com>
2024-03-08 20:14:34 -0400

    refactor: moved game over logic into a method

--- 01_game.rb
+++ 01_game.rb
@@ -1,19 +1,21 @@
 class Game
 
 	def initialize()
-		@current_player = 0
+		@@current_player = 0
+
 		print"Player One Name? >"
 		player_1 = $stdin.gets.chomp
 		print"Player Two Name? >"
 		player_2 = $stdin.gets.chomp
-		@players = [Player.new(player_1), Player.new(player_2)]
+		@@players = [Player.new(player_1), Player.new(player_2)]
 	end
 
+	# determines which players turn
 	def reverse_current
-		if @current_player == 0
-			@current_player = 1
+		if @@current_player == 0
+			@@current_player = 1
 		else
-			@current_player = 0
+			@@current_player = 0
 		end
 	end
 
@@ -22,34 +24,39 @@ class Game
 		num2 = 1 + rand(19)
 		question = [num1, num2, num1 + num2]
 
-		puts "Player #{@players[@current_player].name}: What does #{question[0]} + #{question[1]} equal?"
+		puts "Player #{@@players[@@current_player].name}: What does #{question[0]} + #{question[1]} equal?"
 		print ">"
 		player_answer = $stdin.gets.chomp
 
 		if (player_answer != "#{question[2]}")
-			# subtract current player life
-			puts "Player #{@players[@current_player].name}: Seriously? No!"
+			# subtract current player life when wrong
+			puts "Player #{@@players[@@current_player].name}: Seriously? No!"
+			
+			@@players[@@current_player].lives -= 1
 
-			@players[@current_player].lives -= 1
-
-			if @players[@current_player].lives <= 0
-				reverse_current
-				
-				puts "Player #{@players[@current_player].name} wins with a score of #{@players[@current_player].lives}/3"
-				puts "----- GAME OVER -----"
-				puts "Good bye!"
-				exit(0)
+			if @@players[@@current_player].lives <= 0
+				game_over
 			else
-				puts "P1: #{@players[0].lives}/3 vs P2: #{@players[1].lives}/3"
+				puts "P1: #{@@players[0].lives}/3 vs P2: #{@@players[1].lives}/3"
 			end
 			
 			puts "--- NEW TURN ---"
 		else
-			puts "Player #{@players[@current_player].name}: Yes! You are correct."
+			puts "Player #{@@players[@@current_player].name}: Yes! You are correct."
 			puts "--- NEW TURN ---"
 		end
 		
+		# turn loop
 		reverse_current
 		self.start_turn
+	end
+
+	def game_over
+		reverse_current
+				
+		puts "Player #{@@players[@@current_player].name} wins with a score of #{@@players[@@current_player].lives}/3"
+		puts "----- GAME OVER -----"
+		puts "Good bye!"
+		exit(0)
 	end
 end
